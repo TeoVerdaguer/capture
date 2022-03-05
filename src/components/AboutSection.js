1408f60fd2d5ba587b97f38c8d6a4cb7d9d9b682
Mateo Verdaguer <mateoverdaguer@Mateos-Air.fibertel.com.ar>
2022-03-05 16:23:28 -0300

    styled components

---
+++ src/components/AboutSection.js
@@ -0,0 +1,55 @@
+import React from "react";
+import home1 from "../img/home1.png";
+// Styled
+import styled from 'styled-components';
+
+const AboutSection = () => {
+    return (
+        <About>
+            <Description>
+                <div className="title">
+                    <div className="hide">
+                        <h2>We work to make</h2>
+                    </div>
+                    <div className="hide">
+                        <h2>
+                            your <span>dreams</span> come
+                        </h2>
+                    </div>
+                    <div className="hide">
+                        <h2>true.</h2>
+                    </div>
+                </div>
+                <p>
+                    Contact us for any photography or videography ideas that you
+                    have. We have professionals with amazing skills.
+                </p>
+                <button>Contact Us</button>
+                <div className="image">
+                    <img src={home1} alt="guy with a camera" />
+                </div>
+            </Description>
+        </About>
+    );
+};
+
+// Styled Component
+const About = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`;
+
+const Description = styled.div`
+    flex: 1;
+    padding-right: 5rem;
+    h2 {
+        font-weight: lighter;
+    }
+`;
+
+
+export default AboutSection;
