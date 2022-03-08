ee4bb4bb4c186b975f742c2e1b328ffc7a911694
Mateo Verdaguer <mateoverdaguer123@gmail.com>
2022-03-08 00:25:00 -0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
-// Import pages
-import AboutUs from "./pages/AboutUs";
+//Global Style
 import GlobalStyle from "./components/GlobalStyle";
+//Import Pages
+import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+//Router
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <GlobalStyle />
-      <AboutUs />
-    </div>
-  );
+    return (
+        <div className="App">
+            <GlobalStyle />
+            <Nav />
+            <Routes>
+                <Route path="/" element={<AboutUs />} exact></Route>
+                <Route path="/work" element={<OurWork />}></Route>
+                <Route path="/contact" element={<ContactUs />}></Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,55 +1,39 @@
 import React from "react";
 import home1 from "../img/home1.png";
 // Styled
-import styled from 'styled-components';
+import styled from "styled-components";
+import { About, Description, Image, Hide } from "../styles";
 
 const AboutSection = () => {
     return (
         <About>
             <Description>
                 <div className="title">
-                    <div className="hide">
+                    <Hide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>
                             your <span>dreams</span> come
                         </h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>true.</h2>
-                    </div>
+                    </Hide>
                 </div>
                 <p>
                     Contact us for any photography or videography ideas that you
                     have. We have professionals with amazing skills.
                 </p>
                 <button>Contact Us</button>
-                <div className="image">
-                    <img src={home1} alt="guy with a camera" />
-                </div>
             </Description>
+            <Image>
+                <img src={home1} alt="guy with a camera" />
+            </Image>
         </About>
     );
 };
 
 // Styled Component
-const About = styled.div`
-    min-height: 90vh;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 5rem 10rem;
-    color: white;
-`;
-
-const Description = styled.div`
-    flex: 1;
-    padding-right: 5rem;
-    h2 {
-        font-weight: lighter;
-    }
-`;
-
 
 export default AboutSection;
