21e93e8f0dba5acfb9f17c1bc4d075acbf2d0a24
Mateo Verdaguer <mateoverdaguer123@gmail.com>
2022-03-09 21:58:31 -0300

    update

--- src/movieState.js
+++ src/movieState.js
@@ -79,3 +79,5 @@ export const MovieState = () => {
     },
   ];
 };
+
+export default MovieState;
--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import MovieState from '../movieState';
 
 const MovieDetail = () => {
+  const navigate = useNavigate();
+  const url = navigate.location.pathname;
+  const [movies, setMovies] = useState(MovieState);
+  const [movie, setMovie] = useState(null);
+
+  // Use Effect
+  useEffect(() => {
+    const currentMovie = movies.filter((stateMovie) =>  stateMovie.url === url );
+    setMovie(currentMovie);
+  });
+
   return(
     <div>
       <h1>Movie Detail</h1>
